8012d3df7dee12c3bcf6a470654820cbbfe8b482
Egor Rozhkov <ear17@europlan.ru>
2019-03-07 10:09:48 +0300

    add mvc

---
+++ LanguageFeatures/LanguageFeatures/Controlles/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using LanguageFeatures.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace LanguageFeatures.Controlles
+{
+    public class HomeController : Controller
+    {
+        // GET: /<controller>/
+        public ViewResult Index()
+        {
+            List<string> results = new List<string>();
+
+            foreach (Product p in Product.GetProducts())
+            {
+                string name = p?.Name ?? "<No Name>";
+                decimal? price = p?.Price ?? 0;
+                string relatedName = p?.Relater?.Name ?? "<None>";
+                results.Add(string.Format("Name: {0}, Price: {1}, Related: {2}", name, price, relatedName));
+            }
+            return View(results);
+        }
+    }
+}
---
+++ LanguageFeatures/LanguageFeatures/Models/Product.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LanguageFeatures.Models
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public decimal? Price { get; set; }
+        public Product Relater { get; set; }
+        public static Product[] GetProducts()
+        {
+            Product kayak = new Product
+            {
+                Name = "Kayak",
+                Price = 275M
+            };
+            Product lifejacket = new Product
+            {
+                Name = "lifejacket",
+                Price = 48.95M
+            };
+
+            kayak.Relater = lifejacket; 
+
+            return new Product[] { kayak, lifejacket, null };
+        }
+    }
+}
